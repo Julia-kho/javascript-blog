1e4b61947ba155e99edc8d09c1e941a539d78a9a
Julia Khonina <julia.khonina@gmail.com>
2023-06-01 19:57:54 +0200

    Find the title element

--- js/script.js
+++ js/script.js
@@ -60,19 +60,24 @@ function generateTitleLinks(){
 console.log(generateTitleLinks);
 
   /* remove contents of titleList */
+
   const titleList = document.querySelector(optTitleListSelector);
 
   titleList.innerHTML = '';
 
   /* for each article */
+
   const articles = document.querySelectorAll(optArticleSelector);
 
   for(let article of articles){
   
     /* get the article id */
-    const articleId = getAttribute('id');
+
+    const articleId = document.getAttribute('id');
 
     /* find the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* get the title from the title element */
 
